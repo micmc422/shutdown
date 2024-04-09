3a630d6d8c1630a87a190b66cd155759f2cdb6f2
micmc422 <micmc422@gmail.com>
2024-04-09 17:34:00 +0200

    ref on image

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
     <main className="flex min-h-screen items-center">
       <div className="relative h-screen w-1/2 overflow-clip">
       <Image src={COIN} alt="coin" placeholder="blur" className="object-cover object-bottom" />
+      <div className="absolute bottom-4 left-4 text-xs p-1 bg-black rounded">
+          Photo de <a href="https://unsplash.com/fr/@a_ismail?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ash _ Ismail</a> sur <a href="https://unsplash.com/fr/photos/pieces-rondes-en-or-sur-textile-noir-OLRWjFIRvxY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+      </div>
       </div>
       <div className="relative flex place-items-center">
         <div className="max-w-prose p-24">
